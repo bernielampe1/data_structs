9bc594aec04be3b90c2a9f150615d67d42018087
Your Name <you@example.com>
2020-02-26 11:07:17 -0500

    added move constructor and assignment operator to Array.h

--- Array.h
+++ Array.h
@@ -1,7 +1,6 @@
-#ifndef _BHL_ARRAY_H_
-#define _BHL_ARRAY_H_
+#pragma once
 
-#include<ostream>
+// Rule-of-five implementation of template Array.
 
 template <typename T> class Array {
 private:
@@ -17,13 +16,13 @@ public:
   public:
     iterator(Array<T> *ptr, unsigned p) : _ptr(ptr), _p(p) {}
 
-    bool operator==(const iterator &rhs) const { return (_p == rhs._p); }
-    bool operator!=(const iterator &rhs) const { return (_p != rhs._p); }
-    bool operator<(const iterator &rhs) const { return (_p < rhs._p); }
-    bool operator>(const iterator &rhs) const { return (_p > rhs._p); }
+    bool operator==(const iterator &o) const { return _p == o._p; }
+    bool operator!=(const iterator &o) const { return _p != o._p; }
+    bool operator<(const iterator &o) const { return _p < o._p; }
+    bool operator>(const iterator &o) const { return _p > o._p; }
     iterator &operator++() {
       _p++;
-      return (*this);
+      return *this;
     }
     iterator operator++(int) {
       iterator it = *this;
@@ -32,23 +31,23 @@ public:
     }
     iterator &operator--() {
       _p--;
-      return (*this);
+      return *this;
     }
     iterator operator--(int) {
       iterator it = *this;
       _p--;
-      return (it);
+      return it;
     }
     iterator &operator+=(const int i) {
       _p += i;
-      return (*this);
+      return *this;
     }
     iterator &operator-=(const int i) {
       _p -= i;
-      return (*this);
+      return *this;
     }
-    T &operator*() const { return ((*_ptr)[_p]); }
-    T *operator->() const { return (&(operator*())); };
+    T &operator*() const { return (*_ptr)[_p]; }
+    T *operator->() const { return &(operator*()); };
   };
 
   class const_iterator {
@@ -59,73 +58,87 @@ public:
   public:
     const_iterator(const Array<T> *ptr, unsigned p) : _ptr(ptr), _p(p) {}
 
-    bool operator==(const const_iterator &rhs) const { return (_p == rhs._p); }
-    bool operator!=(const const_iterator &rhs) const { return (_p != rhs._p); }
-    bool operator<(const const_iterator &rhs) const { return (_p < rhs._p); }
-    bool operator>(const const_iterator &rhs) const { return (_p > rhs._p); }
+    bool operator==(const const_iterator &o) const { return _p == o._p; }
+    bool operator!=(const const_iterator &o) const { return _p != o._p; }
+    bool operator<(const const_iterator &o) const { return _p < o._p; }
+    bool operator>(const const_iterator &o) const { return _p > o._p; }
     const_iterator &operator++() {
       _p++;
-      return (*this);
+      return *this;
     }
     const_iterator operator++(int) {
       const_iterator it = *this;
       _p++;
-      return (it);
+      return it;
     }
     const_iterator &operator--() {
       _p--;
-      return (*this);
+      return *this;
     }
     const_iterator operator--(int) {
       const_iterator it = *this;
       _p--;
-      return (it);
+      return it;
     }
     const_iterator &operator+=(const int i) {
       _p += i;
-      return (*this);
+      return *this;
     }
     const_iterator &operator-=(const int i) {
       _p -= i;
-      return (*this);
+      return *this;
     }
-    const T &operator*() const { return ((*_ptr)[_p]); }
-    const T *operator->() const { return (&(operator*())); };
+    const T &operator*() const { return (*_ptr)[_p]; }
+    const T *operator->() const { return &(operator*()); }
   };
 
 public:
-  Array() : _data(new T[0]), _n(0) {}
+  Array() : _data(0), _n(0) {}
 
   Array(const unsigned n) : _data(new T[n]), _n(n) {}
 
   Array(const T &o, const unsigned n) : _data(new T[n]), _n(n) {
-    for (unsigned i = 0; i < _n; i++) {
-      _data[i] = o;
-    }
+    for (unsigned i = 0; i < _n; i++) _data[i] = o;
   }
 
-  Array(const Array &rhs) : _data(new T[rhs._n]), _n(rhs._n) {
-    for (unsigned i = 0; i < _n; i++) {
-      _data[i] = rhs._data[i];
-    }
+  Array(const Array &o) : _data(new T[o._n]), _n(o._n) {
+    for (unsigned i = 0; i < _n; i++) _data[i] = o._data[i];
   }
 
-  ~Array() { delete[] _data; }
+  Array(Array &&o) : _data(o._data), _n(o._n) {
+    o._data = 0;
+    o._n = 0;
+  }
 
-  Array &operator=(const Array &rhs) {
-    if (&rhs != this) {
-      if (_data) {
+  virtual ~Array() { delete[] _data; }
+
+  Array &operator=(const Array &o) {
+    if (&o != this) {
+
+      if (this->_n != o._n) {
         delete[] _data;
+        _data = new T[_n];
+        _n = o._n;
       }
-      _n = rhs._n;
-      _data = new T[_n];
 
-      for (unsigned i = 0; i < _n; i++) {
-        _data[i] = rhs._data[i];
-      }
+      for (unsigned i = 0; i < _n; i++) _data[i] = o._data[i];
     }
 
-    return (*this);
+    return *this;
+  }
+
+  Array &operator=(Array &&o) {
+    if (this != &o) {
+      if (_n) delete [] _data;
+
+      _data = o._data;
+      o._data = 0;
+
+      _n = o._n;
+      o._n = 0;
+    }
+
+    return *this;
   }
 
   const T &operator[](const unsigned i) const { return (_data[i]); }
@@ -133,18 +146,16 @@ public:
   T &operator[](const unsigned i) { return (_data[i]); }
 
   void resize(const unsigned n) {
-    T *_tmpData = new T[n];
+    if (n != _n)  {
+      T *_tmpData = new T[n];
 
-    unsigned m = n < _n ? n : _n;
-    for (unsigned i = 0; i < m; i++) {
-      _tmpData[i] = _data[i];
-    }
+      unsigned m = n < _n ? n : _n;
+      for (unsigned i = 0; i < m; i++) _tmpData[i] = _data[i];
 
-    if (_data) {
-      delete[] _data;
+      if (_data) delete[] _data;
+      _data = _tmpData;
+      _n = n;
     }
-    _data = _tmpData;
-    _n = n;
   }
 
   const T *data() { return (_data); }
@@ -162,14 +173,13 @@ public:
   const_iterator end() const { return (const_iterator(this, _n)); }
 };
 
+#include<ostream>
 template <typename T>
-std::ostream &operator<<(std::ostream &os, const Array<T> &rhs) {
-  for (typename Array<T>::const_iterator it = rhs.begin(); it != rhs.end();
+std::ostream &operator<<(std::ostream &os, const Array<T> &o) {
+  for (typename Array<T>::const_iterator it = o.begin(); it != o.end();
        it++) {
     os << *it << ", ";
   }
 
   return (os);
 }
-
-#endif // _BHL_ARRAY_H_
